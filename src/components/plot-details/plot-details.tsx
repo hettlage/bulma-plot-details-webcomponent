f7a0c3fe1ac48682e6dbb36d191e661fd5d7fa53
Christian Hettlage <hettlage@saao.ac.za>
2018-09-01 22:39:54 +0200

    added plot details component

---
+++ src/components/plot-details/plot-details.tsx
@@ -0,0 +1,13 @@
+import { Component } from '@stencil/core';
+
+@Component({
+  tag: 'saao-plot-details',
+  styleUrl: 'saao-plot-details'
+           })
+export class PlotDetails {
+  render() {
+    return (
+      <div class="saao-plot-details"></div>
+    );
+  }
+}
