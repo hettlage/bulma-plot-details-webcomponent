1aca6da8d8b72ccfcb8b155feca6bbf2569ae0a9
Christian Hettlage <hettlage@saao.ac.za>
2018-09-04 00:06:04 +0200

    show the plot modal when clicking on the plot

--- src/components/plot/plot.spec.ts
+++ src/components/plot/plot.spec.ts
@@ -1,5 +1,6 @@
 import { TestWindow } from '@stencil/core/testing';
 import { Plot } from './plot';
+import { PlotModal } from '../plot-modal/plot-modal';
 
 describe('Plot', () => {
   it('should load', () => {
@@ -23,5 +24,36 @@ describe('Plot', () => {
     it('should render child content', () => {
       expect(element.querySelector('.child-content')).toBeTruthy();
     })
+  });
+
+  it('should show the plot modal when the plot is clicked', async () => {
+    const testWindow = new TestWindow();
+    const element: HTMLElement = await testWindow.load({
+      components: [Plot, PlotModal],
+      html: `<saltastro-plot>
+<div>some plot content</div>
+<saltastro-plot-modal>
+<div>some plot modal content</div>
+</saltastro-plot-modal>
+</saltastro-plot>`
+    });
+
+    // click to display the modal
+    const plotModal = element.querySelector('saltastro-plot-modal');
+    const toggle = jest.fn();
+    Object.defineProperty(plotModal, 'toggle', {value: toggle});
+    const plotDiv = element.querySelector('div');
+    plotDiv.click();
+    expect(toggle).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not fail if there is no plot modal', async ()=> {
+    const testWindow = new TestWindow();
+    const element: HTMLElement = await testWindow.load({
+      components: [Plot],
+      html: '<saltastro-plot></saltastro-plot>'
+                                                       });
+    const plotDiv = element.querySelector('div');
+    plotDiv.click();
   })
 });
--- src/components/plot/plot.tsx
+++ src/components/plot/plot.tsx
@@ -1,13 +1,22 @@
-import { Component } from '@stencil/core';
+import { Component, Element } from '@stencil/core';
 
 @Component({
   tag: 'saltastro-plot',
   styleUrl: 'plot.css'
            })
 export class Plot {
+  @Element() private element: HTMLSaltastroPlotElement;
+
+  toggleModal = () => {
+    Array.from(this.element.querySelectorAll('saltastro-plot-modal'))
+      .forEach((modal) => modal.toggle());
+  };
+
   render() {
     return (
+      <div onClick={this.toggleModal}>
         <slot/>
+      </div>
     );
   }
 }
